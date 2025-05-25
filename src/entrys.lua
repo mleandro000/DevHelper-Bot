aeac0d0acbb9476ee434ebfb48ddbe4025fceb21
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-24 21:53:53 -0300

    fix: add error handling for invalid entry paths

--- src/entrys.lua
+++ src/entrys.lua
@@ -5,12 +5,13 @@ function configure_entries(llm)
     local total_entries = argv.get_flag_size({ "entries","e" })
     for i = 1,total_entries do
         local current_entrie = argv.get_flag_arg_by_index({ "entries","e" }, i)
+       
         if dtw.isfile(current_entrie) then
             text_to_insert = "file: " .. current_entrie.."\n"
             text_to_insert = text_to_insert.. dtw.load_file(current_entrie) .. "\n"
             text_to_insert = text_to_insert .."-----------------\n"
         end 
-        if dtw.isdir(current_entrie) then
+       elseif dtw.isdir(current_entrie) then
             local all_files = dtw.list_files_recursively(current_entrie,true)
             for j=1,#all_files do
                 local current_file = all_files[j]
@@ -19,6 +20,9 @@ function configure_entries(llm)
                 text_to_insert = text_to_insert .."-----------------\n"
             end
         end
+        else 
+            error("current entrie "..current_entrie.." its not a file or a dir")
+        end 
     end 
     llm.add_system_prompt(text_to_insert)
 end
